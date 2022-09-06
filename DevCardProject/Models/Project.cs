e2f9da573e2efd188f18c1c5070ba0a54c394741
AmirrezaVajdy <Amirreza.vajdy@gmail.com>
2022-09-05 22:53:25 -0700

    add Prject View Component and Dynamic Project List

--- DevCardProject/Models/Project.cs
+++ DevCardProject/Models/Project.cs
@@ -5,7 +5,16 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
         public string Client { get; set; }
 
+        public Project(int id, string title, string description, string image, string client)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+            Client = client;
+        }
     }
 }
---
+++ DevCardProject/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using DevCardProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCardProject.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        
+        public IViewComponentResult Invoke(string name)
+        {
+            List<Project> projects = new()
+            {
+                new Project(1, "ماشین حساب", "یک ماشین حساب پیشرفته روز دنیا", "project-1.jpg", "International"),
+
+            };
+            return View("_Projects", projects);
+        }
+    }
+}
