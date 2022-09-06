454fddec2a0e6d9c8d545aeceb95f45e2fcf1248
AmirrezaVajdy <Amirreza.vajdy@gmail.com>
2022-09-05 23:14:23 -0700

    add Articles to ViewComponent and Dynamic Article

---
+++ DevCardProject/ViewComponents/ArticlesViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DevCardProject.Models;
+namespace DevCardProject.ViewComponents
+{
+    public class ArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<Article> articles = new List<Article>()
+            {
+                new Article(1 , "Asp.net core 7 preview" , "بلاخره بعد از انتشار دات نت 6 همه علاقه مندان منتظر منتشر دات نت 7 بودند که اخر سر مایکروسافت ان را منتشر کرد" ,"blog-post-thumb-1.jpg")
+            };
+            return View("_Articles", articles);
+        }
+    }
+}
